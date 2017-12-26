19e8d8fc645c0d5d69e49cedf3658909f018b096
denis-skripnik <scadens@yandex.ru>
2017-12-26 09:15:21 +0700

    Увеличение скорости загрузки страницы главной
    Была - 5.9, стала - 1.8 секунды.

--- newbie-list.php
+++ newbie-list.php
@@ -1,13 +1,13 @@
 <?php
 echo '<ul>';
-foreach($pagedData AS $item) {
+foreach($result AS $item) {
 echo '<li><a href="https://goldvoice.club/@'.$item['author'].'" target="_blank">'.$item['author'].'</a></li>';
 }
 echo '</ull>';
-if((40 > $currentPage && ($currentPage - 1) < $numPages) && ($currentPage > 0)) {
-    echo '<ul><li><a href="?page=' . ($currentPage - 1) . '">« Предыдущая страница</a></li></ul>';
+if((40 > $_GET['page'] && ($_GET['page'] - 1) < 40) && ($_GET['page'] > 0)) {
+    echo '<ul><li><a href="?page=' . ($_GET['page'] - 1) . '">« Предыдущая страница</a></li></ul>';
 }
-if(40 > $currentPage && ($currentPage + 1) < $numPages) {
-    echo '<ul><li><a href="?page=' . ($currentPage + 1) . '" class="right">Следующая страница »</a></li></ul>';
+if(40 > $_GET['page'] && ($_GET['page'] + 1) < 40) {
+    echo '<ul><li><a href="?page=' . ($_GET['page'] + 1) . '" class="right">Следующая страница »</a></li></ul>';
 }
 ?>
--- newbie-posts.php
+++ newbie-posts.php
@@ -1,36 +1,18 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-include 'golos/getnewbies.php';
-$url = "http://185.203.243.142/api/?method=getnewbies&count=1200";
+if ($_GET['page'] >=1) {
+$url_page = "http://185.203.243.142/api/?method=getnewbies&count=30";
+$url = $url_page."&offset=".$_GET['page']*30;
+}
+else {
+$url = "http://185.203.243.142/api/?method=getnewbies&count=30&offset=0";
+}
 $curl = curl_init($url); // Инициализируем curl по указанному адресу
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // Записать http ответ в переменную, а не выводить в буфер
 $page = curl_exec($curl);
 $result = json_decode($page, true);
-// Получаем текущую страницу
-if (!empty($_REQUEST['page'])) {
-$currentPage = trim($_REQUEST['page']);
-    $currentPage = (int) $currentPage;
-} else {
-    $currentPage = 0;
-}
-// Настройки разбиения на страницы   
-$perPage = 30;
-$numPages = ceil(1200 / $perPage);
-if(!$currentPage || $currentPage > $numPages) {
-    $currentPage = 0;
-}
-$start = $currentPage * $perPage;
-$end = ($currentPage * $perPage) + $perPage;
-
-// Нужные страницы
-foreach ($result as $key => $value) {
-    if($key >= $start && $key < $end) {
-        $pagedData[] = $result[$key];
-    }
-}
-
 echo '<table><tr><th>Автор</th><th>Заголовок поста с ссылкой на этом сайте</th><th>Ссылка на golos.io</th><th>Ссылка на goldvoice</th><th>Голосов</th></tr>';
-foreach($pagedData AS $item) {
+foreach($result AS $item) {
 echo '<tr>
         <td><a href="https://goldvoice.club/@'.$item['author'].'" target="_blank">@'.$item['author'].'</a></td>
         <td><h2><a href="/post.php?permlink='.$item['permlink'].'&author='.$item['author'].'">'.$item['title'].'</a></h2></td>
@@ -40,10 +22,10 @@ echo '<tr>
     </tr>';
 }
 echo '</table>';
-if((40 > $currentPage && ($currentPage - 1) < $numPages) && ($currentPage > 0)) {
-    echo '<ul><li><a href="?page=' . ($currentPage - 1) . '">« Предыдущая страница</a></li></ul>';
+if((40 > $_GET['page'] && ($_GET['page'] - 1) < 40) && ($_GET['page'] > 0)) {
+    echo '<ul><li><a href="?page=' . ($_GET['page'] - 1) . '">« Предыдущая страница</a></li></ul>';
 }
-if(40 > $currentPage && ($currentPage + 1) < $numPages) {
-    echo '<ul><li><a href="?page=' . ($currentPage + 1) . '" class="right">Следующая страница »</a></li></ul>';
+if(40 > $_GET['page'] && ($_GET['page'] + 1) < 40) {
+    echo '<ul><li><a href="?page=' . ($_GET['page'] + 1) . '" class="right">Следующая страница »</a></li></ul>';
 }
 ?>
